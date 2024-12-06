4af8ec6b0751cbc2bc94e6134e0acb71ce800068
enricorenna01 <erenna39@gmail.com>
2024-12-06 17:23:37 +0100

    feat: added variables js and corrected html

--- JS/main.js
+++ JS/main.js
@@ -1,1 +1,12 @@
-console.log("sono connesso");
+/*DICHIARAZIONE VARIABILI*/
+const workingHours = 10;
+const backendDevelopment = 20.50;
+const frontendDevelopment = 15.30;
+const projectAnalysis = 33.60;
+const promotionalCode = [
+    "YHDNU32",
+    "JANJC63",
+    "PWKCN25",
+    "SJDPO96",
+    "POCIE24",
+];
