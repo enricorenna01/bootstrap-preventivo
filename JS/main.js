9da9e2677e7c57e3edf8c2e6007977e5c26edf93
enricorenna01 <erenna39@gmail.com>
2024-12-06 20:26:00 +0100

    feat: fixed html and added price calculation

--- JS/main.js
+++ JS/main.js
@@ -1,8 +1,10 @@
 /*DICHIARAZIONE VARIABILI*/
 const workingHours = 10;
-const backendDevelopment = 20.50;
-const frontendDevelopment = 15.30;
-const projectAnalysis = 33.60;
+const price = {
+   backend: 20.50,
+   frontend: 15.30,
+   analysis: 33.60
+};
 const promotionalCode = [
     "YHDNU32",
     "JANJC63",
@@ -10,3 +12,35 @@ const promotionalCode = [
     "SJDPO96",
     "POCIE24",
 ];
+
+/*PRELEVO DAL DOM IL TIPO DI LAVORO*/
+const myForm = document.getElementById("form");
+
+
+/*EVENTO CAUSATO AL PREMERE DEL PULSANTE*/
+myForm.addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    /*CALCOLO DEL PREZZO*/
+    let job = document.getElementById("inputSelect").value;
+    let jobPrice;
+    console.log(job);
+    if (job === "1") {
+        jobPrice = price.backend;
+    
+    } else if (job === "2") {
+        jobPrice = price.frontend;
+    
+    } else {
+        jobPrice = price.analysis;
+    
+    };
+    console.log(jobPrice);
+    let finalPrice = jobPrice * workingHours
+    console.log(finalPrice);
+
+
+
+
+
+});
